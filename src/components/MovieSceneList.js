bc119f0d589d377e8ea21aca245e6f0b594bf6b6
GemaMesasV <gema.mesas.v@gmail.com>
2022-05-07 22:15:36 +0200

    add conditional no movie name finded

--- src/components/MovieSceneList.js
+++ src/components/MovieSceneList.js
@@ -1,5 +1,9 @@
 import MovieSceneItem from './MovieSceneItem';
 function MovieSceneList(props) {
+	if (props.movieList.length=== 0) {
+		return (<p>Oops! There is not movie with that name!</p>)
+		
+	}
   return (
     <ul className="movie_list">
       {props.movieList.map((movie) => (
